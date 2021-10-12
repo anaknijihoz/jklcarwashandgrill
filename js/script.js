4e6a67ec3140cb8430351a59c6e47d052855874e
Son of Jihoz <91451910+anaknijihoz@users.noreply.github.com>
2021-10-12 19:32:43 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -68,3 +68,29 @@ var offsetEnd = 0;
 window.addEventListener('scroll', () => {
   document.documentElement.style.setProperty('--scroll', ( window.pageYOffset - offsetStart ) / ( document.documentElement.offsetHeight - offsetStart - offsetEnd - window.innerHeight ));
 }, false);
+
+
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
