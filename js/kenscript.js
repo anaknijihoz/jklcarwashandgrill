a710374464bea15299676fe460359184e4e3c4d7
Son of Jihoz <91451910+anaknijihoz@users.noreply.github.com>
2021-10-25 11:58:55 +0800

    Update kenscript.js

--- js/kenscript.js
+++ js/kenscript.js
@@ -21,6 +21,9 @@ window.onscroll = () => {
 
 }
 
+
+
+
 function loader() {
     document.querySelector('.loader-container').classList.add('fade-out');
 }
@@ -100,3 +103,35 @@ var swiper = new Swiper(".review-slider", {
   {
 AOS.init();
   });
+
+
+
+
+
+  
+  (function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+            || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+
